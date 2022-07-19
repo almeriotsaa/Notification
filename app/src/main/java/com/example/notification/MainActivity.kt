aad6630e748d1823d195fc974069ccf3c1f0e021
Almerio Tsany <almerio.tsany@gmail.com>
2022-07-19 14:41:26 +0700

    Custom Notif with Status Progress

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.core.app.NotificationCompat
 import com.example.notification.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private var notificationManager: NotificationManager? = null
+    private var CHANNEL_ID = "channel_id"
+
+    private lateinit var countDownTimer: CountDownTimer
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +26,55 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Register channel kedalam android system
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(CHANNEL_ID, "Countdown", "This Is Description")
+        binding.btnStart.setOnClickListener {
+            countDownTimer.start()
+        }
 
+        countDownTimer = object : CountDownTimer(10000, 1000){
+            override fun onTick(p0: Long) {
+                // masukin text dari string.xml
+                binding.timer.text = getString(R.string.time_reamining, p0/1000)
+            }
+
+            override fun onFinish() {
+                displayNotification()
+            }
+
+        }
+
+    }
+
+    private fun displayNotification(){
+
+        val intent = Intent(this, SecondActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+
+
+        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentTitle("MenuToday")
+            .setContentText("0%")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .setProgress(100, 0, false)
+            .setContentIntent(PendingIntent.getActivity(this, 0, intent, 0))
+
+        notificationManager?.notify(1, builder.build())
+    }
+
+    private fun createNotificationChannel(id: String, name: String, channelDescription: String){
+        // validasi akan dibuat jika versi android >= API 26+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = channelDescription
+            }
+            notificationManager?.createNotificationChannel(channel)
+        }
     }
 }
